b5dff8432796571978f130288ef30c86ea01b9b4
Jonatan Cerezuela López <jcerezuelalopezz@gmail.com>
2018-10-17 20:56:29 +0200

    Funcionando

--- tabla/tabla.php
+++ tabla/tabla.php
@@ -5,26 +5,47 @@
     <title>Tabla </title>
   </head>
   <body>
-    <?php $numero = 8 ?>
-    <table border="1">
-      <thead>
-        <th><?= $numero ?></th>
-        <th>x</th>
-        <th>n</th>
-        <th> = </th>
-        <th>m</th>
-      </thead>
-      <tbody>
-        <?php for ($i=0; $i <= 10 ; $i++):?>
-          <tr>
-            <td><?= $numero  ?></td>
-            <td>x</td>
-            <td><?= $i ?></td>
-            <td> = </td>
-            <td><?= $numero * $i  ?></td>
-          </tr>
-        <?php endfor ?>
-      </tbody>
-    </table>
+    <?php
+    function mostrarTabla($numero)
+    {
+      ?>
+        <table border="1">
+          <thead>
+            <th><?= $numero ?></th>
+            <th>x</th>
+            <th>n</th>
+            <th> = </th>
+            <th>m</th>
+          </thead>
+          <tbody>
+            <?php for ($i=0; $i <= 10 ; $i++):?>
+              <tr>
+                <td><?= $numero  ?></td>
+                <td>x</td>
+                <td><?= $i ?></td>
+                <td> = </td>
+                <td><?= $numero * $i  ?></td>
+              </tr>
+            <?php endfor ?>
+          </tbody>
+        </table>
+        <?php
+      }
+      function mostrarError($mensaje)
+      {
+        echo "<h3 style=color: #936;>$mensaje</h3>";
+      }
+
+      if (!isset($_GET['num'])){
+         mostrarError("Error: falta el parámetro num");
+      } else {
+        $numero = $_GET['num'];
+        if (!ctype_digit($numero)){
+           mostrarError('Error: se ha pasado algo que no es un número');
+         } else {
+          mostrarTabla($numero);
+        }
+      }
+    ?>
   </body>
 </html>
