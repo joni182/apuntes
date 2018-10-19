916cb2cf31731f8bd03abea48b575eb99b3c99e6
Jonatan Cerezuela López <jcerezuelalopezz@gmail.com>
2018-10-19 20:00:15 +0200

    Integra los dos archivos en uno

--- tabla/index.php
+++ tabla/index.php
@@ -5,15 +5,18 @@
     <title>Tabla </title>
   </head>
   <body>
+    <form action="" method="get">
+      <label for="num">Número</label>
+      <input id="num" type="text" name="num" >
+      <input type="submit" name="" value="Calcular">
+    </form>
     <?php
     require './auxiliar.php';
-      if (!isset($_GET['num'])){
-         mostrarError("Error: falta el parámetro num");
-      } else {
+      if (isset($_GET['num'])){
         $numero = $_GET['num'];
         if (!ctype_digit($numero)){
            mostrarError('Error: se ha pasado algo que no es un número');
-         } else {
+        } else {
           mostrarTabla($numero);
         }
       }
